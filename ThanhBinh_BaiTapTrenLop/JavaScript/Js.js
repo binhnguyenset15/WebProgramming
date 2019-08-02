1cd1b790e3a1c7c8d065bfd7ffb17a0ebc23b107
Bnh Nguyen <binh.nguyen.set15@eiu.edu.vn>
2019-08-02 11:16:19 +0700

    .

--- ThanhBinh_BaiTapTrenLop/JavaScript/Js.js
+++ ThanhBinh_BaiTapTrenLop/JavaScript/Js.js
@@ -1,3 +1,58 @@
+SinhVien = function(ho,ten){
+	this.ho = ho;
+	this.ten = ten;
+}
+var sinhvien=[
+	new SinhVien('ben','le1'),
+	new SinhVien('ben','le2'),
+	new SinhVien('ben','le3'),
+	new SinhVien('ben','le4'),
+	new SinhVien('ben','le5'),
+]
+function inDanhSachV2(){
+	for (sv in sinhvien){
+		console.log();
+	}
+}
+function inne(sv){
+	for (let i in sv) {
+		document.write(sv[i]+ " ");
+	}
+}
+function isThisExist(sv, ten){
+	var fIndex=sv.indexOf(ten)
+	if(fIndex >=0 ){
+		document.write("<br><b>Có </b>"+ ten +"<b> nha</b><br>");
+	}else{
+		document.write("<br><b>Hong có </b>"+ ten +"<b> nha</b><br>");
+	}
+}
+function inDanhSachSV(){
+	var sv = ['ben','binh','ben1','binh1','tam']
+		inne(sv)
+	document.write("<br>");
+	for (var i = 0; i < sv.length; i++) {
+		if(i==2){
+			sv[i]='tenmoi';
+		}
+	}
+	inne(sv);
+	document.write("<br><b> thêm anh ben2</b><br>")
+	sv.push("ben2");
+	inne(sv);
+	document.write("<br><b> xóa anh ben2 vì ngu</b><br>")
+	sv.pop(5);
+	inne(sv);
+	document.write("<br><b> cho anh ben2 lên đầu tại đã khôn lên</b><br>")
+	sv.unshift("ben2");
+	inne(sv);
+	document.write("<br><b> xóa anh ben2 ở đầu vì ngu</b><br>")
+	sv.shift();
+	inne(sv);
+	isThisExist(sv, "ben2")
+}
+
+
 function TinhTongLe() {
 	var a =1;
 	var tong=0;
