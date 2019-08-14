5f6bbb970a5fc2ceb772b8438795d31616e5d735
Bnh Nguyen <binh.nguyen.set15@eiu.edu.vn>
2019-08-14 14:15:16 +0700

    .

--- ThanhBinh_BaiTapLab/Lab7/Lab7 Js.js
+++ ThanhBinh_BaiTapLab/Lab7/Lab7 Js.js
@@ -36,6 +36,8 @@ function hideForm() {
 function createNewSV() {
     var hoten = $("input[name=HoTen]").val();
     var MSSV = $("input[name=MSSV]").val();
+    $("input[name=HoTen]").val('');
+    $("input[name=MSSV]").val('');
     var NSV = new SinhVien(hoten, MSSV);
     sinhViens.unshift(NSV);
     ShowSinhVien();
@@ -49,8 +51,12 @@ function createNewSV() {
 //    }
 //}
 
+var NoC = 0;
+var sortName = 0;
+var SortMS = 0;
 
 $(function () {
+    $("input").prop("required", "true");
     $('.DSSV').on('click','.deleteRow', function () {
         var MSSV = $(this).attr('id');
         var index = sinhViens.findIndex(x => x.MSSV == MSSV);
@@ -60,7 +66,7 @@ $(function () {
     $('.DSSV').on('click', '.editRow', function () {
         var MSSV = $(this).attr('id');
         var index = sinhViens.findIndex(x => x.MSSV == MSSV);
-        $('#' + MSSV).parents('.row').html('<div class="col-1" >' + (+index + 1) + '</div ><div class="col-4"><input type="text" name="reHoTen" placeholder="' + sinhViens[index].HoTen + '"/></div><div class="col-4"><input type="text" name="reMSSV" placeholder="' + sinhViens[index].MSSV +'"/></div><div class="col-3"><button class="btnSave">Accept</button><button class="Cancel" onclick="ShowSinhVien()" >Cancel</button></div>');
+        $('#' + MSSV).parents('.row').html('<div class="col-1" >' + (+index + 1) + '</div ><div class="col-4"><input type="text" name="reHoTen" placeholder="' + sinhViens[index].HoTen + '"/></div><div class="col-4"><input type="number" name="reMSSV" placeholder="' + sinhViens[index].MSSV +'"/></div><div class="col-3"><button class="btnSave">Accept</button><button class="Cancel" onclick="ShowSinhVien()" >Cancel</button></div>');
         $('.btnSave').on('click', function () {
             var reHT = $('input[name=reHoTen]').val();
             var reMSSV = $('input[name=reMSSV]').val();
@@ -68,6 +74,42 @@ $(function () {
             sinhViens[index] = reSV;
             ShowSinhVien();
         });
+    });
+    $('.setBG').on('click', function () {
+        NoC += 1;
+        if (NoC == 1)
+            $('body').css('background-color', 'coral');
+        if (NoC == 2)
+            $('body').css('background-color', '#99ff99');
+        if (NoC == 3) { 
+            $('body').css('background-color', '#99ccff');
+            NoC = 0;
+        }
+        $(".rsBG").css("display", "block")
+    });
+    $('.rsBG').on('click', function () {
+        $('body').css('background-color', 'white');
+        $(".rsBG").css("display", "none");
+    });
+    $('.sortTen').on('click', function () {
+        sortName +=1
+        if (sortName %2 !=0) {
+            sinhViens.sort((a, b) => (a.HoTen > b.HoTen) ? 1 : ((b.HoTen > a.HoTen) ? -1 : 0));
+        }
+        if (sortName %2 == 0) {
+            sinhViens.sort((a, b) => (a.HoTen < b.HoTen) ? 1 : ((b.HoTen < a.HoTen) ? -1 : 0));
+        }
+        ShowSinhVien();
+    });
+    $('.sortMSSV').on('click', function () {
+        SortMS += 1;
+        if (SortMS % 2 != 0) {
+            sinhViens.sort((a, b) => (a.MSSV > b.MSSV) ? 1 : ((b.MSSV > a.MSSV) ? -1 : 0));
+        }
+        if (SortMS % 2 == 0) {
+            sinhViens.sort((a, b) => (a.MSSV > b.MSSV) ? -1 : ((b.MSSV > a.MSSV) ? 1 : 0));
+        }
+        ShowSinhVien();
     });
 });
 
